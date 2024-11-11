ad673b05c3f38625e9dbd9970bacbdcc469e53ab
kashanafzl <kashanafzal2221@gmail.com>
2024-11-11 14:43:07 +0500

    navbar

--- src/Components/Home/Navbar/Navbar.jsx
+++ src/Components/Home/Navbar/Navbar.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import './Navbar.css'
+
+
+import logo from '../../../Assets/Img/logo.png'
+import calenderimg from '../../../Assets/Img/calenderimg.svg'
+import notifactionimg from '../../../Assets/Img/notifaction.svg'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <div>
+
+      <div className='loginandsignupmainparentbtndiv'>
+
+        <button>Login In</button>
+        <button>Signup</button>
+
+
+        <img src={calenderimg} alt="" />
+        <img src={notifactionimg} alt="" />
+      </div>
+
+    </div>
   )
 }
